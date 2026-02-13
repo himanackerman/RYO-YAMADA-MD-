848b1333fcd4b9156f792a32b65c998dc4fa09aa
Hilman <himanackerman@gmail.com>
2026-02-13 14:36:13 +0700

    Anu

---
+++ plugins/anime-picre.js
@@ -0,0 +1,43 @@
+// Image Picre random
+// API : https://rynekoo-api.hf.space/
+// Author : Hilman 
+
+let handler = async (m, { conn }) => {
+  try {
+
+    await m.react('✨')
+
+    const res = await fetch('https://rynekoo-api.hf.space/random/pic-re')
+
+    if (!res.ok) throw new Error('Fetch gagal')
+
+    const buffer = Buffer.from(await res.arrayBuffer())
+
+    await conn.sendMessage(m.chat, {
+      image: buffer,
+      caption: '_Nih hehe_',
+      footer: 'ʀyᴏ yᴀᴍᴀᴅᴀ - ᴍᴅ',
+      buttons: [
+        {
+          buttonId: '.picre',
+          buttonText: { displayText: '🔁 Lagi' },
+          type: 1
+        }
+      ],
+      headerType: 4
+    }, { quoted: m })
+
+  } catch (e) {
+    console.error(e)
+    await m.react('❌')
+    m.reply('❌ Error mengambil gambar.')
+  }
+}
+
+handler.help = ['picre']
+handler.tags = ['anime']
+handler.command = /^picre$/i
+handler.limit = true
+handler.group = false
+
+export default handler
